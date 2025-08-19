b01ee74e02296864f9e40e815a68a702287d13d6
zoranjuras <cmundir@gmail.com>
2025-08-19 16:52:13 +0200

    added increment, decrement and remove buttons to the cart

--- src/app/components/cart-details/cart-details.component.ts
+++ src/app/components/cart-details/cart-details.component.ts
@@ -42,4 +42,16 @@ export class CartDetailsComponent {
 
   }
 
+  incrementQuantity(theCartItem: CartItem) {
+    this.cartService.addToCart(theCartItem);
+  }
+
+  decrementQuantity(theCartItem: CartItem) {
+    this.cartService.decrementQuantity(theCartItem);
+  }
+
+  remove(theCartItem: CartItem) {
+    this.cartService.remove(theCartItem);
+  }
+
 }
