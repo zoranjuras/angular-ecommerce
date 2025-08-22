2a2893b3fb48a68d2da0388413b452443f98d3a9
zoranjuras <cmundir@gmail.com>
2025-08-22 13:03:46 +0200

    Added checkout component, added route to it and created the new checkout button and link to checkout component

---
+++ src/app/components/checkout/checkout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout',
+  standalone: false,
+  templateUrl: './checkout.component.html',
+  styleUrl: './checkout.component.css'
+})
+export class CheckoutComponent {
+
+}
