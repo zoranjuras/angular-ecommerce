5437c47801467b5be0debeb9525eb78cab054fec
zoranjuras <cmundir@gmail.com>
2025-08-24 12:59:11 +0200

    Added form controls for shipping and address, and credit card. Added checkbox for copying shipping address to billing address.

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -22,8 +22,42 @@ export class CheckoutComponent implements OnInit {
         firstName: [''],
         lastName: [''],
         email: ['']
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMoth: [''],
+        expirationYear: ['']
       })
+
     });
+  }
+
+  copyShippingAddressToBillingAddress(event) {
+
+    if (event.target.checked) {
+      this.checkoutFormGroup.controls['billingAddress']
+      .setValue(this.checkoutFormGroup.controls['shippingAddress'].value);      
+    }
+    else {
+      this.checkoutFormGroup.controls['billingAddress'].reset();
+    }
   }
 
   onSubmit() {
